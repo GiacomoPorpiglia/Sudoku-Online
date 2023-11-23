3073bc33f3f56e93aa1584327a1ba7fcefcea9c7
Giacomo Porpiglia <giacomo.porpiglia@gmail.com>
2023-11-23 17:31:38 +0100

    Server code refactored in 'server' folder

--- client/src/components/BottomBanner.js
+++ client/src/components/BottomBanner.js
@@ -4,7 +4,7 @@ const BottomBanner = () => {
   return (
     <div className='bottom-banner'>
         Created by Giacomo Porpiglia, see the GitHub source for the project
-        <a href='https://github.com/GiacomoPorpiglia/sudokuOnline'> here</a>
+        <a href='https://github.com/GiacomoPorpiglia/sudoku-online'> here</a>
     </div>
   )
 }

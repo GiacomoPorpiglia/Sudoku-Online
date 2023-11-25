f5d716d38080320b5811c048f5373ee62c646e8e
Giacomo Porpiglia <giacomo.porpiglia@gmail.com>
2023-11-25 10:08:26 +0100

    connected client to server

--- server/index.js
+++ server/index.js
@@ -21,9 +21,9 @@ app.use('/api/v1/auth', userAuthRouter)
 app.use('/api/v1/statistics', statisticsRouter)
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+    app.use(express.static('../client/build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
     })
 }
 
